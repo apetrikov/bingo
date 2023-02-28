79bbf306a95b506cb548cc169d6bcd789afda86b
apetrikov <jnmort@gmail.com>
2023-02-28 22:14:21 +0300

    feat(board): define win line

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,26 +1,34 @@
 import React, {useState} from 'react';
 import styles from './Board.module.css';
 import {Tile} from './Tile'
-import {generateGrid, Cell} from "./utils";
+import {generateGrid, Cell, getWinnerCells} from "./utils";
 
 type Props = {
-    phraseArray: string[]
+    phraseArray: string[],
+    cols?: number
 }
 
-export function Board({phraseArray}: Props) {
-  const [grid, setGrid] = useState(generateGrid(phraseArray))
+export function Board({phraseArray, cols}: Props) {
+    const [grid, setGrid] = useState(generateGrid(phraseArray))
     const handleClick = (el: Cell) => () => {
         const newState = [...grid]
         newState[el.id].clicked = true
         // TODO check winner
+      const winnerCells: Cell[] = getWinnerCells(el.id, newState, cols)
+        if (winnerCells.length) winnerCells.forEach(i => newState[i.id].winner = true)
         setGrid(newState)
-        console.log(el) //  TODO call api handler
+//        console.log(el) //  TODO call api handler
     }
 
     return (
         <div className={styles.board}>
-          {grid.map((el, i) => <Tile key={el.id} text={el.text} clicked={el.clicked} cb={handleClick(el)}
-                                       central={el.central}/>)}
+            {grid.map((el, i) => <Tile key={el.id}
+                                       text={el.text}
+                                       clicked={el.clicked}
+                                       cb={handleClick(el)}
+                                       central={el.central}
+                                       winner={el.winner}
+            />)}
         </div>
     );
 }
--- src/components/Board/utils.ts
+++ src/components/Board/utils.ts
@@ -16,3 +16,49 @@ export function generateGrid(array: string[]): Cell[] {
     winner: false
   })))
 }
+
+// The grid is always a square!
+export function getWinnerCells(updatedIndex: number, grid: Cell[], cols = 5): Cell[] {
+  if (updatedIndex < 0) return []
+  if (updatedIndex > grid.length - 1) return []
+  if (grid.length < cols * cols) return []
+
+  const cellsToCheck: Record<string, Cell[]> = {
+    row: [],
+    columns: [],
+    diagonal1: [],
+    diagonal2: []
+  }
+
+  const firstRowIndex = Math.floor(updatedIndex / cols)
+  cellsToCheck.row = grid.slice(firstRowIndex, firstRowIndex + cols)
+
+  const firstColIndex = updatedIndex % cols
+  for (let i = 0; i < cols; i++) {
+    const element = grid[firstColIndex + i * cols]
+    cellsToCheck.columns.push(element)
+  }
+
+  for (let i = 0; i < cols; i++) {
+    const element1 = grid[i + i * cols]
+    cellsToCheck.diagonal1.push(element1)
+
+    const element2 = grid[cols - 1 + i * (cols - 1)]
+    cellsToCheck.diagonal2.push(element2)
+  }
+
+  const checkIndexes = (indexes: Cell[]): boolean => indexes.every(i => i.clicked)
+
+  // The game stops after the first win line
+  const isRowWinner = checkIndexes(cellsToCheck.row)
+  const isColWinner = checkIndexes(cellsToCheck.columns)
+  const isDiagonal1Winner = checkIndexes(cellsToCheck.diagonal1)
+  const isDiagonal2Winner = checkIndexes(cellsToCheck.diagonal2)
+
+  if (isRowWinner) return cellsToCheck.row
+  if (isColWinner) return cellsToCheck.columns
+  if (isDiagonal1Winner) return cellsToCheck.diagonal1
+  if (isDiagonal2Winner) return cellsToCheck.diagonal2
+
+  return []
+}
