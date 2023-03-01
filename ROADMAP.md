5e6f37a65cb63e1a62d57262512e289286b35e69
apetrikov <31124509+apetrikov@users.noreply.github.com>
2023-03-01 18:17:23 +0300

    Feature/winner (#2)
    * feat(board): define winner algo

--- ROADMAP.md
+++ ROADMAP.md
@@ -1,8 +1,8 @@
 
-1. Set up react + ts
-2. Create a bingo from JSON
-3. Add game logic
-4. Add winner reward
+1. + Set up react + ts
+2. + Create a bingo from array
+3. + Add game logic
+4. + Add winner reward
 5. Think about fair play
 6. Think about design
 7. Think about new features
--- src/components/Board/utils.ts
+++ src/components/Board/utils.ts
@@ -1,18 +1,56 @@
 export type Cell = {
-  id: number,
-  text: string,
-  clicked: boolean,
-  central: boolean,
-  winner: boolean,
+    id: number,
+    text: string,
+    clicked: boolean,
+    central: boolean,
+    winner: boolean,
 }
 
 export function generateGrid(array: string[]): Cell[] {
-  const centralIndex = Math.floor(array.length / 2)
-  return array.map(((text, index) => ({
-    id: index,
-    text,
-    clicked: index === centralIndex,
-    central: index === centralIndex,
-    winner: false
-  })))
+    const centralIndex = Math.floor(array.length / 2)
+    return array.map(((text, index) => ({
+        id: index,
+        text,
+        clicked: index === centralIndex,
+        central: index === centralIndex,
+        winner: false
+    })))
+}
+
+// The grid is always a square!
+export function getWinnerCells(updatedIndex: number, grid: Cell[], cols = 5): Cell[] {
+    if (updatedIndex < 0) return []
+    if (updatedIndex > grid.length - 1) return []
+    if (grid.length < cols * cols) return []
+
+    const cellsToCheck: Record<string, Cell[]> = {
+        row: [],
+        columns: [],
+        diagonal1: [],
+        diagonal2: []
+    }
+
+    const firstRowIndex = Math.floor(updatedIndex / cols) * cols
+    cellsToCheck.row = grid.slice(firstRowIndex, firstRowIndex + cols)
+
+    const firstColIndex = updatedIndex % cols
+    for (let i = 0; i < cols; i++) {
+        const element = grid[firstColIndex + i * cols]
+        cellsToCheck.columns.push(element)
+    }
+
+    for (let i = 0; i < cols; i++) {
+        const element1 = grid[i + i * cols]
+        cellsToCheck.diagonal1.push(element1)
+
+        const element2 = grid[cols - 1 + i * (cols - 1)]
+        cellsToCheck.diagonal2.push(element2)
+    }
+
+    const checkIndexes = (indexes: Cell[]): boolean => indexes.every(i => i.clicked)
+
+    return Object
+        .values(cellsToCheck)
+        .filter(checkIndexes)
+        .flat()
 }
